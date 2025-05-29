68aa8a6cf1566b0359efdb76de6bb4afbba953bc
safwat266 <mzayed251@gmail.com>
2025-05-29 12:15:19 +0300

    first commit

--- ooo.dart
+++ ooo.dart
@@ -1,3 +1,30 @@
-void main(){
+class BankAccount {
+  double balance = 0;
+
+  double get getBalance {
+    return balance;
+  }
+
   
+  set deposit(double amount) {
+    balance += amount;
+  }
+
+  
+  set withdraw(double amount) {
+    if (amount <= balance) {
+      balance -= amount;
+    } else {
+      print(' balance. ');
+    }
+  }
+}
+
+void main(List<String> arguments) {
+  BankAccount bankAccount = BankAccount();
+
+  bankAccount.deposit = 5000;
+  print("After deposit: ${bankAccount.getBalance}");
+  bankAccount.withdraw = 200;
+  print("After withdrawal: ${bankAccount.getBalance}");
 }
